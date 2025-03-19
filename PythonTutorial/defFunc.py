f920135660de622623ed1738a374deaafc321392
yaoyongpeng <yaoyongpeng@qq.com>
2025-03-19 17:36:21 +0800

    section comment refined

--- PythonTutorial/defFunc.py
+++ PythonTutorial/defFunc.py
@@ -1,4 +1,4 @@
-#-4.8Define Functions---
+#-------------------------------------4.8Define Functions---------------------------
 def fib(n):
     """Print a Fibonacci series less than n."""
     a,b=0,1
@@ -12,7 +12,7 @@ def fib(n):
 
 print(fib(100))
 
-#4.9.1 Default Argument
+#-------------------------------4.9.1 Default Argument-------------------------------------------
 def ask_ok(prompt,retries=4,reminder="Please try again!"):
     while True:
         reply=input(prompt)
@@ -30,7 +30,7 @@ ask_ok('OK to overwrite the file?', 2)
 ask_ok('OK to overwrite the file?', 2, 'Come on, only yes or no!')
 
 
-#4.9.2 keyword argument
+#-------------------------------4.9.2 keyword argument------------------------------
 def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
     print("-- This parrot wouldn't", action, end=' ')
     print("if you put", voltage, "volts through it.")
@@ -76,7 +76,7 @@ cheeseshop("Limburger", "It's very runny, sir.",
            client="John Cleese",
            sketch="Cheese Shop Sketch")
 
-#-4.9.3. Special parameters
+#-----------------------------------4.9.3. Special parameters--------------------------------------
 #For readability and performance, it makes sense to restrict the way arguments can be passed 
 # so that a developer need only look at the function definition to determine 
 # if items are passed by position, by position or keyword, or by keyword.
@@ -116,14 +116,14 @@ combined_example(1, 2, kwd_only=3)  #√
 combined_example(1, standard=2, kwd_only=3) #√
 #combined_example(pos_only=1, standard=2, kwd_only=3)    #runtime TypeError:combined_example() got some positional-only arguments passed as keyword arguments: 'pos_only'
 
-#4.9.4. Arbitrary Argument Lists
+#-------------------4.9.4. Arbitrary Argument Lists-----------------------------------
 def concat(*args, sep="/"):
     return sep.join(args)
 
 print(concat("earth", "mars", "venus"))
 print(concat("earth", "mars", "venus", sep="."))#*name parameters,can only be followed by keyword parameter.
 
-#-4.9.5. Unpacking Argument Lists
+#--------------------4.9.5. Unpacking Argument Lists-----------------------------------
 #The reverse situation occurs when the arguments are already in a list or tuple but need to be unpacked for a function call 
 # requiring separate positional arguments. For instance, the built-in range() function expects separate start and stop arguments. 
 # If they are not available separately, write the function call with the *-operator to unpack the arguments out of a list or tuple:
@@ -155,7 +155,7 @@ def my_function():
 
 print(my_function.__doc__)
 
-#4.9.8. Function Annotations
+#-----------------4.9.8. Function Annotations--------------------------------------
 def f(ham:str, eggs:str="eggs")->str:
     """Demo annotations
 
